e636eaf8af8891a35114fc3612a1e3bc56abe690
“TTp95” <studio.pipe.95@gmail.com>
2023-04-29 01:41:16 -0400

    source file remap

--- nvim_lua/.config/nvim/lua/ttp/remap.lua
+++ nvim_lua/.config/nvim/lua/ttp/remap.lua
@@ -52,7 +52,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- source current file
-vim.keymap.set("n", "<leader><leader>", function()
+vim.keymap.set("n", "<leader><leader>so", function()
     vim.cmd("so")
 end)
 
